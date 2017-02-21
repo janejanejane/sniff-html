2eb15b214541b4dbafc994a642282f2e836acb3c
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2017-02-21 22:32:41 +1300

    WIP Inject api url

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,9 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin( {
+      __API__: apiHost,
+    } ),
   ],
   module: {
     loaders: [{
@@ -28,3 +31,21 @@ module.exports = {
     extensions: ['', '.js', '.jsx', '.scss', '.css', '.html'],
   },
 };
+
+// @link: http://developmentnow.com/2016/07/13/webpack-injecting-server-urls/
+// @link: http://stackoverflow.com/questions/36102751/how-set-the-base-path-of-rest-api-with-webpack
+let apiHost;
+
+const setupAPI = function () {
+  switch ( process.env.NODE_ENV ) {
+    case 'production':
+      apiHost = 'http://prod.com';
+      break;
+    case 'development':
+    default:
+      apiHost = 'http://dev.com';
+      break;
+  }
+};
+
+setupAPI();
